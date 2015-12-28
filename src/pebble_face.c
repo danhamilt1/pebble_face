3fab2dda16eb6181397c36cb8749a643c12c063b
danhamilt1 <daniel2.hamilton@live.uwe.ac.uk>
2015-12-28 13:38:01 +0000

    Added readme

--- src/pebble_face.c
+++ src/pebble_face.c
@@ -2,6 +2,7 @@
 
 static Window *s_main_window;
 static TextLayer *s_time_layer;
+static GFont s_time_font;
 
 static void update_time() {
   // Get a tm struct
@@ -32,10 +33,12 @@ static void main_window_load(Window *window) {
     GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50)
   );
 
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
+
   // Improce the layout to be more like a watchface
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorRed);
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_font(s_time_layer, s_time_font);
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
@@ -43,11 +46,15 @@ static void main_window_load(Window *window) {
 static void main_window_unload(Window *window) {
   // Destroy TextLayer
     text_layer_destroy(s_time_layer);
+
+    fonts_unload_custom_font(s_time_font);
 }
 
 static void init() {
   // Create main Window element and assign to pointer
     s_main_window = window_create();
+
+    resource_init_current_app(&APP_RESOURCES);
 
     // Set handlers to manage the elements inside the Window
     window_set_window_handlers(s_main_window, (WindowHandlers) {
