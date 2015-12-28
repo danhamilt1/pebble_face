2291a7d5e831534b69856e222527aa35d14e4a70
danhamilt1 <daniel2.hamilton@live.uwe.ac.uk>
2015-12-28 12:07:25 +0000

    initial commit, basic watchface

---
+++ src/pebble_face.c
@@ -0,0 +1,77 @@
+#include <pebble.h>
+
+static Window *s_main_window;
+static TextLayer *s_time_layer;
+
+static void update_time() {
+  // Get a tm struct
+  time_t temp = time(NULL);
+  struct tm *tick_time = localtime(&temp);
+
+  // Write the current hours and minutes into a buffer
+  static char s_buffer[8];
+  strftime(s_buffer, sizeof(s_buffer),
+    clock_is_24h_style() ? "%H:%M" : "%I:%M", tick_time
+  );
+
+  // Display time
+  text_layer_set_text(s_time_layer, s_buffer);
+}
+
+static void tick_handler(struct tm *tick_time, TimeUnits units_changed){
+  update_time();
+}
+
+static void main_window_load(Window *window) {
+  // Get information about the window
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+
+  // Create the TextLayer with specific bounds
+  s_time_layer = text_layer_create(
+    GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50)
+  );
+
+  // Improce the layout to be more like a watchface
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorRed);
+  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
+}
+
+static void main_window_unload(Window *window) {
+  // Destroy TextLayer
+    text_layer_destroy(s_time_layer);
+}
+
+static void init() {
+  // Create main Window element and assign to pointer
+    s_main_window = window_create();
+
+    // Set handlers to manage the elements inside the Window
+    window_set_window_handlers(s_main_window, (WindowHandlers) {
+      .load = main_window_load,
+      .unload = main_window_unload
+    });
+
+    // Register with TickTimerService
+    tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+
+    // Show the Window on the watch, with animated=true
+    window_stack_push(s_main_window, true);
+
+    // Init correct time
+    update_time();
+}
+
+static void deinit() {
+  // Destroy Window
+  window_destroy(s_main_window);
+}
+
+int main(void) {
+  init();
+  app_event_loop();
+  deinit();
+}
